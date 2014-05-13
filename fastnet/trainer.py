977a3cf73d9320539ee597a522c0e6ff357a6a01
sainaa <tesatory@yahoo.com>
2014-05-12 23:24:33 -0400

    get confusion

--- fastnet/trainer.py
+++ fastnet/trainer.py
@@ -461,6 +461,30 @@ class Trainer:
     print >> sys.stderr, '---- test ----'
     print >> sys.stderr, 'top 1 error: %f | top 5 error: %f' % (1 - total_correct, 1 - total_correct_top5)
 
+  def get_confusion(self):
+    if self.net.layers[-2].name == 'noise':
+      clear_w = gpuarray.to_gpu(np.eye(self.net.layers[-2].weight.shape[0], dtype = np.float32))
+      noisy_w = self.net.layers[-2].weight
+      self.net.layers[-2].weight = clear_w
+
+    num_classes = self.net.layers[-1].outputSize
+    conf = np.zeros((num_classes, num_classes), dtype = np.float32)
+    while self.curr_epoch < 2:
+      test_data = self.test_dp.get_next_batch(self.batch_size)
+      input, label = test_data.data, test_data.labels
+      self.net.train_batch(input, label, TEST)
+      self.curr_epoch = test_data.epoch
+      self.curr_batch += 1
+      out = self.net.output.get()
+      label = label.get()
+      for i in xrange(label.shape[0]):
+        conf[:, int(label[i])] += out[:,i]
+
+    if self.net.layers[-2].name == 'noise':
+      self.net.layers[-2].weight = noisy_w
+    conf = conf / conf.sum(axis = 0, keepdims = True)
+    return conf
+
   def report(self):
     rep = self.net.get_report()
     if rep is not None:
